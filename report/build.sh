181540c131cf74041386b64870ad6c0d10fa0d82
Brandon Uehlein <uehlbran@isu.edu>
2019-05-31 19:01:03 +0000

    got working

--- report/build.sh
+++ report/build.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-markdown-pp report.mdpp -o report.md
-pandoc report.md --template=templates/ieee.latex --top-level-division=section --pdf-engine=xelatex -F pandoc-crossref -F pandoc-citeproc -f markdown+fenced_code_attributes -f markdown+multiline_tables -o report.pdf
+set -e
+markdown-pp report.mdpp -e latexrender -o report.gen.md
+
+# indent abstract so it is properly formatted in the abstract.
+sed -e "s/^/    /" < abstract.md > indented-abstract.gen.md
+markdown-pp metadata.yml.mdpp -o metadata.gen.yml
+pandoc report.gen.md \
+  --metadata-file=metadata.gen.yml \
+  --template=../templates/ieee.latex \
+  --top-level-division=section \
+  --bibliography=../ISUESE.json \
+  --csl=../ieee.csl \
+  -F pandoc-crossref \
+  -f markdown+fenced_code_attributes \
+  -f markdown+multiline_tables \
+  -f markdown+tex_math_single_backslash \
+  -o report.gen.tex
+xelatex report.gen
---
+++ report/report.md
@@ -0,0 +1,37 @@
+
+
+# Introduction
+
+## Problem Statement
+
+## Research Objectives
+
+## Context
+
+## Organization
+
+# Background and Related Work {#sec:background}
+
+# Current {#sec:current}
+
+# Alternative {#sec:alt}
+
+# Contributions {#sec:contrib}
+
+# Research Methods {#sec:methods}
+
+## Search Strategy and Screening {#sec:strategy}
+
+## Establishing a Classification Scheme {#sec:establish}
+
+## Mapping of Studies and Analysis {#sec:mapping_of}
+
+      !INCLUDE "scheme.md"
+      !INCLUDE "mapping.md"
+      
+\printbibliography
+
+\appendix
+
+# Appendices
+
